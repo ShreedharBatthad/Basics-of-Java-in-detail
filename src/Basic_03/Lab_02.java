d82b8c3d49aa892e4edc3cc05120f8ebc50abb56
shreedhar <shreedharbe1997@gmail.com>
2024-12-21 23:38:55 +0530

    Third Commit

--- src/Basic_03/Lab_02.java
+++ src/Basic_03/Lab_02.java
@@ -4,6 +4,17 @@ public class Lab_02
 {
     public static void main(String[] args)
     {
+              int a = 10;
+              int b = 34;
+              String Bird = "Pigeon";
 
+              System.out.println(a+b);
+              System.out.println(a);
+
+              //BODMAS RULE
+              // LEFT TO RIGHT
+              System.out.println(a+b+Bird);
+              System.out.println(a+Bird+b);
+              System.out.println(Bird+a+b);
     }
 }
