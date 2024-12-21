5c3d0d10072bd1b991e6fccb819d8b4bfe1ed488
shreedhar <shreedharbe1997@gmail.com>
2024-12-21 13:00:48 +0530

    Second Commit

--- src/Lab7.java
+++ src/Lab7.java
@@ -2,31 +2,34 @@ public class Lab7
 {
     public static void main(String[] args)
     {
-        boolean Powerison = true;
-        int voltage = 200;
+        boolean powerison = true;
+        int voltage = 240;
 
-        if (voltage > 250)
+
+        if(powerison)
         {
-            System.out.println("High Voltage");
+            if(voltage==240)
+            {
+                System.out.println("Normal Voltage");
+                System.out.println("Washing Machine will start running");
+            }
+            else if (voltage<240 && voltage>230)
+            {
+                System.out.println("Low Voltage");
+                System.out.println("Washing Machine will run slowly!");
+            }
+            else if (voltage>300)
+            {
+                System.out.println("High Voltage");
+                System.out.println("Washing Machine will stop");
+            }
+            else {
+                System.out.println("Voltage is out of range");
+            }
         }
-        if (voltage == 240)
-        {
-            System.out.println("Normal Voltage");
+    else{
+            System.out.println("Washing machine is off");
         }
-        if (voltage < 250) {
-            System.out.println("Low Voltage");
-        }
-
-        if (Powerison)
-        {
-            if (Powerison)
-            System.out.println("WashingMachine will start");
-        }
-        else
-            System.out.println("WashingMachine is Off");
-
-
     }
-
-    }
+}
 
