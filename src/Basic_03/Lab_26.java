4c1e40d3c2fba793cb0132e406bd0b01c5c20da5
Shreedhar <shreedharbatthad@gmail.com>
2024-12-24 09:39:51 +0530

    Commit

--- src/Basic_03/Lab_26.java
+++ src/Basic_03/Lab_26.java
@@ -1,9 +1,33 @@
 package Basic_03;
 
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
 public class Lab_26
 {
     public static void main(String[] args)
     {
+       Scanner sc = new Scanner(System.in);
+       System.out.println("Enter Browser name");
+       String browser = sc.next();
 
+       switch (browser)
+       {
+           case "chrome":
+               System.out.println("Chrome Browser is Executed");
+           break;
+           case "opera":
+               System.out.println("Opera Browser is Executed");
+           break;
+           case "firefox":
+               System.out.println("Firefox Browser is Executed");
+           break;
+           case "edge" :
+               System.out.println("Edge Browser is Executed");
+           break;
+           default:
+               System.out.println("Oops Invaild Browser Name!");
+
+       }
     }
 }
