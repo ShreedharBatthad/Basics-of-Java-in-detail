47469a8604c22bd0bf160e561e968ece65fd2638
Shreedhar <shreedharbatthad@gmail.com>
2024-12-29 21:26:21 +0530

    Commit

---
+++ src/IterativeStatements/Lab_56.java
@@ -0,0 +1,15 @@
+package IterativeStatements;
+
+public class Lab_56
+{
+    public static void main(String[] args)
+    {
+         long a = 0;
+
+         for(int i=0; i<10; i++)
+         {
+             a =a+i;
+         }
+        System.out.println(a);
+    }
+}
