a5c8f4dd3d8c0a0b2a6da28fea3c3ad1df1894db
Shreedhar <shreedharbatthad@gmail.com>
2024-12-29 15:54:30 +0530

    Commit

---
+++ src/IterativeStatements/Lab_51.java
@@ -0,0 +1,14 @@
+//package IterativeStatements;
+//
+//public class Lab_51
+//{
+//    public static void main(String[] args)
+//
+////    {
+////         int a =10;
+////         if(a==10)
+////         {
+////             break;
+////         }
+////    }
+////}
---
+++ src/IterativeStatements/Lab_55.java
@@ -0,0 +1,11 @@
+package IterativeStatements;
+
+public class Lab_55
+{
+    public static void main(String[] args)
+    {
+
+
+
+    }
+}
