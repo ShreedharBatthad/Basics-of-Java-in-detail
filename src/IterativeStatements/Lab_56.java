755c810703ae7fef2a7a1d834d52ac1e6fefa3dd
Shreedhar <shreedharbatthad@gmail.com>
2024-12-29 22:53:41 +0530

    Commit

--- src/IterativeStatements/Lab_56.java
+++ src/IterativeStatements/Lab_56.java
@@ -4,12 +4,16 @@ public class Lab_56
 {
     public static void main(String[] args)
     {
+        // sum of total numbers
          long a = 0;
+         int count;
 
          for(int i=0; i<10; i++)
          {
-             a =a+i;
+             a=a+i;
+
          }
         System.out.println(a);
+
     }
 }
