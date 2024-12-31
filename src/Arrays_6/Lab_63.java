8a64b6a99cd9f1dc2c0bc812a2c1a28a0ae9a356
Shreedhar <shreedharbatthad@gmail.com>
2024-12-31 12:59:53 +0530

    Commit

--- src/Arrays_6/Lab_63.java
+++ src/Arrays_6/Lab_63.java
@@ -7,6 +7,7 @@ public class Lab_63
         //Arrays
         //Array is used to store multiple values in a single variable.
         //example
+        //Single Dimension Array
         String [] cars = {"Benz", "BMW", "Ford"};
         // here cars is variable, that too single one.
     }
--- src/Arrays_6/Lab_67.java
+++ src/Arrays_6/Lab_67.java
@@ -3,6 +3,9 @@ package Arrays_6;
 public class Lab_67
 {
     public static void main(String[] args) {
+
+
+        //Single Dimension Array
         //Loops for the Array.
         String [] Bird = {"Parrot", "Pigeon", "Peacock", "Crane"};
         for(int i=0; i<Bird.length; i++)
