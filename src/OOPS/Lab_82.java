65be01d02e1e0ec0c2161cb9ec854a4f24859b93
Shreedhar <shreedharbatthad@gmail.com>
2025-01-02 15:30:03 +0530

    New Commit

---
+++ src/OOPS/Lab_82.java
@@ -0,0 +1,14 @@
+package OOPS;
+
+public class Lab_82
+{
+    public static void main(String[] args)
+    {
+        print("Hello");
+    }
+    static void print(String s) /// void -> it will not return any value, because void is present.
+    {
+        System.out.println(s);
+
+    }
+}
