b2573a2470b5363c62a3db30b54b06861275ce72
Shreedhar <shreedharbatthad@gmail.com>
2025-01-03 02:41:01 +0530

    New Commit

---
+++ src/OOPS/ConstructorExample_87.java
@@ -0,0 +1,26 @@
+package OOPS;
+
+public class ConstructorExample_87
+{
+    //Declaration.
+    int days ;
+    String Course ;
+
+
+    ConstructorExample_87(int i , String c) //Parameterised Constructor.
+    {
+        this.days=  i;   // this keyword refers to the Child Class.
+        this.Course = c;
+    }
+
+    public static void main(String[] args)
+    {
+        //Object Creation.
+        ConstructorExample_87 Student = new ConstructorExample_87(10,"Java");
+
+        // Calling Variables using the object reference and Printing them using SOP.
+        System.out.println("Duration of th course is:  " +Student.days+ " " +"Programming Language name is: "+Student.Course);
+
+
+    }
+}
