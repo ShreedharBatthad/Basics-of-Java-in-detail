95448634755016a012c2da3f494b37447cfdc08d
Shreedhar <shreedharbatthad@gmail.com>
2025-01-04 23:19:22 +0530

    New Commit

--- src/BJavaPrograms_05/FirstLastnumber.java
+++ src/BJavaPrograms_05/FirstLastnumber.java
@@ -7,14 +7,17 @@ public class FirstLastnumber
         int[] numbers = {10, 20, 30, 40, 50};
 
         // Check if the array is not empty
-        if (numbers.length > 0) {
+        if (numbers.length > 0)
+        {
             int firstElement = numbers[0]; // First element
             int lastElement = numbers[numbers.length - 1]; // Last element
 
             // Display the results
             System.out.println("First element: " + firstElement);
             System.out.println("Last element: " + lastElement);
-        } else {
+        }
+        else
+        {
             System.out.println("The array is empty.");
         }
     }
