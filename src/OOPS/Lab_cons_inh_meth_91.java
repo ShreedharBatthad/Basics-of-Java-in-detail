c027bc98936e0571e7fe1f4ba3152104a4906f04
Shreedhar <shreedharbatthad@gmail.com>
2025-01-18 02:01:48 +0530

    New Commit

--- src/OOPS/Lab_cons_inh_meth_91.java
+++ src/OOPS/Lab_cons_inh_meth_91.java
@@ -2,39 +2,38 @@ package OOPS;
 
 public class Lab_cons_inh_meth_91
 {
-    //variables
-    String brand;
-    int speed;
+           //variables
+           String brand;
+           int speed;
 
-    //constructor
-    Lab_cons_inh_meth_91(String brand, int speed)
-    {
+           //constructor
+           Lab_cons_inh_meth_91(String brand, int speed)
+          {
 
-        this.brand = brand;
-        this.speed = speed;
-    }
-    // methods
-    void displayinfo()
-    {
-        System.out.println("car:" + brand);
-        System.out.println("speed:" + speed);
-
-    }
+              this.brand = brand;
+              this.speed = speed;
+          }
+         // methods
+         void displayinfo()
+          {
+               System.out.println("car:" + brand);
+               System.out.println("speed:" + speed);
+         }
 }
   //extends is a keyword used to achieve inheritance.
 class car extends Lab_cons_inh_meth_91
 {
-     //variable
-    String color;
+         //variable
+            String color;
 
-    //constructor
-    car(String brand, int speed, String color)
-         {
-             //super keyword is used to call the parent class variables.
-              super(brand, speed);
-              this.color = color;
+        //constructor
+          car(String brand, int speed, String color)
+                 {
+                //super keyword is used to call the parent class variables.
+                      super(brand, speed);
+                      this.color = color;
 
-         }
+                }
     //method: set on code is used to perform particular task.
          void displaycarinfo()
 
---
+++ src/OOPS/Lab_method_overloading_92.java
@@ -0,0 +1,23 @@
+package OOPS;
+
+public class Lab_method_overloading_92
+{
+
+    //methodoverloading: having same class with same method name but with diff arguments
+     void show()
+           {
+               System.out.println("Not a Parameterized method");
+           }
+    void show(int a)
+          {
+              System.out.println("Parameterized method");
+          }
+
+    public static void main(String[] args)
+         {
+         Lab_method_overloading_92 load = new Lab_method_overloading_92();
+         load.show(10);
+         load.show();
+
+        }
+}
