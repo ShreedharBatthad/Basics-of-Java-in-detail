35238eb697438b04c6747e7378ef0f7e2f9bf156
Shreedhar <shreedharbatthad@gmail.com>
2025-01-18 01:04:42 +0530

    New Commit

---
+++ src/OOPS/Lab_cons_inh_meth_91.java
@@ -0,0 +1,57 @@
+package OOPS;
+
+public class Lab_cons_inh_meth_91
+{
+    //variables
+    String brand;
+    int speed;
+
+    //constructor
+    Lab_cons_inh_meth_91(String brand, int speed)
+    {
+
+        this.brand = brand;
+        this.speed = speed;
+    }
+    // methods
+    void displayinfo()
+    {
+        System.out.println("car:" + brand);
+        System.out.println("speed:" + speed);
+
+    }
+}
+  //extends is a keyword used to achieve inheritance.
+class car extends Lab_cons_inh_meth_91
+{
+     //variable
+    String color;
+
+    //constructor
+    car(String brand, int speed, String color)
+         {
+             //super keyword is used to call the parent class variables.
+              super(brand, speed);
+              this.color = color;
+
+         }
+    //method: set on code is used to perform particular task.
+         void displaycarinfo()
+
+                 {
+                     //parent class calling method
+                     displayinfo();
+                     System.out.println("color: " + color);
+
+                 }
+
+    public static void main(String[] args)
+    {
+        //object creation.
+        car c = new car("Benz", 150, "Red");
+        c.displaycarinfo();
+    }
+
+
+}
+
