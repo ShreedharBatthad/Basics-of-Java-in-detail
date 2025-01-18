0e081947c04fdb35d060df8913ecb3f5b557cb94
Shreedhar <shreedharbatthad@gmail.com>
2025-01-18 21:56:42 +0530

    New Commit

---
+++ src/OOPS/Lab_methodoverload_case_1__95.java
@@ -0,0 +1,25 @@
+package OOPS;
+
+public class Lab_methodoverload_case_1__95
+{
+    // method 1
+    void show(int a)
+    {
+        System.out.println("Integer method is executed");
+    }
+
+    //method 2
+    void show(String a)
+    {
+        System.out.println("String method is executed");
+    }
+
+
+    public static void main(String[] args)
+    {
+        Lab_methodoverload_case_1__95 load = new Lab_methodoverload_case_1__95();
+        load.show(10);
+
+    }
+
+}
---
+++ src/OOPS/Lab_overload_main_94.java
@@ -0,0 +1,17 @@
+package OOPS;
+
+public class Lab_overload_main_94
+
+{
+// main method
+    public static void main(String[] args)
+    {
+        Lab_overload_main_94 man = new Lab_overload_main_94();
+        man.main(1);
+    }
+// overloaded main method.
+    public static void main (int a)
+    {
+        System.out.println("second main method");
+    }
+}
